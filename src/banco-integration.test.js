a7a7535c39893cdea74adb958c48737260dbcdc7
Lucas Mendes <lucas.mendes.1234@hotmail.com>
2025-08-25 22:17:03 -0300

    attv

--- src/banco-integration.test.js
+++ src/banco-integration.test.js
@@ -10,3 +10,41 @@ test("Deve reornar a conta pelo nome",function(){
     const conta = bancoService.getAccountByName("Lucas")
     expect(conta.nome).toBe('Lucas')
 })
+
+test("Deve listar todas as contas criadas", () => {
+  const contas = bancoService.listAccounts();
+  
+  expect(contas.length).toBe(1);
+  expect(contas).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({ nome: "Lucas" }),
+    ])
+  );
+});
+
+// ATIVIDADE
+
+test("Teste de nome obrigatorio",function(){
+    const novaConta = bancoService.createAccount(null,2000,22)
+    expect(novaConta).toBe("nome é obrigatório")
+})
+
+test("Teste de saldo negativo",function(){
+    const novaConta = bancoService.createAccount("Lucas",-100,22)
+    expect(novaConta).toBe("saldo inicial não pode ser negativo")
+})
+
+test("Test do nome ADMIN",function(){
+    const novaConta = bancoService.createAccount("admin",2000,22)
+    expect(novaConta).toBe("nome de usuário não pode ser admin")
+})
+
+test("Teste de idade minima",function(){
+    const novaConta = bancoService.createAccount("Lucas",2000,17)
+    expect(novaConta).toBe("idade mínima é 18 anos")
+})
+
+test("Teste de nome obrigatorio do GET",function(){
+    const conta = bancoService.getAccountByName(null)
+    expect(conta).toBe("nome é obrigatório")
+})
